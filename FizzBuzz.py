8eac2d2813fb5fe49505eca327121658694b6b2f
Evan Harlan <evanharlan10@gmail.com>
2024-03-07 15:11:28 -0600

    Implemented the FizzBuzz code based on requirements.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,3 +2,12 @@
 # Multiples of 3 - print "Fizz"
 # Multiples of 5 - print "Buzz"
 # Multiples of 3 & 5 - print "FizzBuzz"
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
