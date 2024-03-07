052a98da0cfe041897b26a747aea72ddd9075f87
Evan Harlan <evanharlan10@gmail.com>
2024-03-07 15:23:20 -0600

    Slightly optimized the program by combining divisibility of 3 & 5 into one statement. Also improved the readability of the program as well as comments.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,13 +1,20 @@
-# Write a python program that numbers 1 - 100
-# Multiples of 3 - print "Fizz"
-# Multiples of 5 - print "Buzz"
-# Multiples of 3 & 5 - print "FizzBuzz"
+#-----------------------------#
+#                             #
+#       FizzBuzz Program      #
+#                             #
+#-----------------------------#
+
+# For loop used to print numbers from 1 - 100
 for number in range(1, 101):
-    if number % 3 == 0 and number % 5 == 0:
+    # If the number is divisible by 15 (3 & 5) - print "FizzBuzz"
+    if number % 15 == 0:
         print("FizzBuzz")
+    # Else if the number is ONLY divisible by 3 - print "Fizz"
     elif number % 3 == 0:
         print("Fizz")
+    # Else if the number is ONLY divisible by 5 - print "Buzz"
     elif number % 5 == 0:
         print("Buzz")
+    # Else print the number if it does not meet any above conditions
     else:
         print(number)
